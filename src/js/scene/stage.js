0dc3ae21cd925c8a97e7744d320e7f7cd8bdd7d7
sairoutine <sairoutine@gmail.com>
2017-06-09 21:51:56 +0900

    update: move player

--- src/js/scene/stage.js
+++ src/js/scene/stage.js
@@ -17,17 +17,59 @@ util.inherit(SceneTitle, base_scene);
 SceneTitle.prototype.init = function(){
 	base_scene.prototype.init.apply(this, arguments);
 
-	var player = new Player(this);
-	player.init(this.core.width/2, this.core.height/2);
+	this.player = new Player(this);
+	this.player.init(this.core.width/2, this.core.height/2);
 
-	this.addObject(player);
+	this.addObject(this.player);
 
 };
 
 SceneTitle.prototype.beforeDraw = function(){
 	base_scene.prototype.beforeDraw.apply(this, arguments);
 
+	/*
+	// Zが押下されていればショット生成
+	if(this.core.isKeyDown(Constant.BUTTON_Z)) {
+		this.player.shot();
+	}
+	*/
 
+	var MOVE_NUM = 4;
+
+	// 自機移動
+	if(this.core.isKeyDown(CONSTANT.BUTTON_LEFT)) {
+		this.player.x(this.player.x() - MOVE_NUM);
+	}
+	if(this.core.isKeyDown(CONSTANT.BUTTON_RIGHT)) {
+		this.player.x(this.player.x() + MOVE_NUM);
+	}
+	if(this.core.isKeyDown(CONSTANT.BUTTON_DOWN)) {
+		this.player.y(this.player.y() + MOVE_NUM);
+	}
+	if(this.core.isKeyDown(CONSTANT.BUTTON_UP)) {
+		this.player.y(this.player.y() - MOVE_NUM);
+	}
+
+	/*
+	// 画面外に出させない
+	character.forbidOutOfStage();
+	*/
+
+	/*
+	// 左右の移動に合わせて自機のアニメーションを変更
+	if(this.game.isKeyDown(Constant.BUTTON_LEFT) && !this.game.isKeyDown(Constant.BUTTON_RIGHT)) {
+		// 左移動中
+		character.animateLeft();
+	}
+	else if(this.game.isKeyDown(Constant.BUTTON_RIGHT) && !this.game.isKeyDown(Constant.BUTTON_LEFT)) {
+		// 右移動中
+		character.animateRight();
+	}
+	else {
+		// 左右には未移動
+		character.animateNeutral();
+	}
+	*/
 };
 
 
